3af769cd3ab0a05b7be6404f3a7c2738dfeecfb5
Daniel Gruno <humbedooh@apache.org>
2023-11-05 09:36:21 -0600

    Add cookie-based user sesssion read/write wrappers

---
+++ src/asfquart/session.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""ASFQuart - User session methods and decorators"""
+from . import base
+import quart.sessions
+import time
+
+
+def read(expiry_time=86400*7):
+    """Fetches a cookie-based session if found (and valid), and updates the last access timestamp
+    for the session."""
+    # We store the session cookie using the base.APP.app_id identifier, to distinguish between
+    # two asfquart apps running on the same hostname.
+    cookie_id = base.APP.app_id
+    if cookie_id in quart.session:
+        now = time.time()
+        cookie_expiry_deadline = now - expiry_time
+        session_dict = quart.session[cookie_id]
+        if isinstance(session_dict, dict):
+            session_update_timestamp = session_dict.get("uts", 0)
+            # If a session cookie has expired (not updated/used for seven days), we delete it instead of returning it
+            if session_update_timestamp < cookie_expiry_deadline:
+                del quart.session[cookie_id]
+            # If it's still valid, use it
+            else:
+                # Update the timestamp, since the session has been requested (and thus used)
+                session_dict["uts"] = now
+                return session_dict
+
+
+def write(session_data: dict):
+    """Sets a cookie-based user session for this app"""
+    cookie_id = base.APP.app_id
+    dict_copy = session_data.copy()  # Copy dict so we don't mess with the original data
+    dict_copy["uts"] = time.time()   # Set last access timestamp for expiry checks later
+    quart.session[cookie_id] = dict_copy
