af434691c8544878e5159d6a3b73149de8365c60
Daniel Gruno <humbedooh@apache.org>
2023-11-06 09:02:36 -0600

    Tweak import paths for pytest, fix constructor

--- tests/session.py
+++ tests/session.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
 import sys
-sys.path.append(".")
+sys.path.extend(('src', '../src',))  # Depending on where unit tests are run from, path may differ
 
 import pytest
 import time
 import quart
-from src import asfquart
+import asfquart
 
 
 @pytest.mark.session
 def test_sessions():
-    asfquart.init("foobar")
+    asfquart.construct("foobar")
     quart.session = {asfquart.APP.app_id: {"uts": time.time(), "foo": "bar"}}
     my_session = asfquart.session.read()
     assert my_session, "Was expecting a session, but got nothing in return"
