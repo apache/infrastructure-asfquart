59e6f0ec4eb3de19682ad86e50085e98a2ad1aab
Daniel Gruno <humbedooh@apache.org>
2024-02-14 13:56:02 +0100

    enforce_login should not enforce if the request has an Auth header
    We only enforce login redirects for oauth sessions.

--- src/asfquart/generics.py
+++ src/asfquart/generics.py
@@ -105,7 +105,7 @@ def enforce_login(redirect_uri="/auth"):
     @asfquart.APP.errorhandler(asfquart.auth.AuthenticationFailed)
     async def auth_redirect(error):
         # If we have no client session, redirect to auth flow
-        if not await asfquart.session.read():
+        if not quart.request.authorization and not await asfquart.session.read():
             return quart.redirect(f"{redirect_uri}?login={quart.request.full_path}")
         # If we have a session, but still no access, just say so in plain text.
         return quart.Response(status=error.errorcode, response=error.message)
