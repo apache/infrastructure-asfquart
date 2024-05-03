6a9738a1ea454866d51c2bcbea517f12a418ad83
Greg Stein <gstein@gmail.com>
2024-05-02 19:02:48 -0500

    Remove dynamic() and a few imports.

--- src/asfquart/config.py
+++ src/asfquart/config.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
+
 """ASFQuart - Configuration readers"""
-import signal
-import asyncio
+
 import yaml
 import functools
 import inspect
-import os
-
-from . import base
 
 DEFAULT_CONFIG_FILENAME = "config.yaml"
 
@@ -33,23 +30,3 @@ def static(func):
         await _read_config(func, config_filename)
 
     return config_wrapper
-
-
-def dynamic(func):
-    """Reloadable configuration parser. On startup, or when SIGUSR2 is intercepted, the configuration is reloaded and
-    passed to the configuration callback function."""
-    # If running test suite, the event loop needs to be reset
-    if "PYTEST_CURRENT_TEST" in os.environ and base.loop.is_closed():
-        base.loop = asyncio.get_event_loop()
-
-    @functools.wraps(func)
-    async def config_wrapper_dynamic(config_filename=DEFAULT_CONFIG_FILENAME):
-        def new_task():
-            return asyncio.create_task(_read_config(func, config_filename))
-
-        # Add a signal handler for SIGUSR2
-        base.loop.add_signal_handler(signal.SIGUSR2, new_task)
-        # Read the config on first run
-        await _read_config(func, config_filename)
-
-    return config_wrapper_dynamic
