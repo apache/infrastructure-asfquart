6fde8ba868c57bfb69da99afd8563b23db0e2a1a
Arnout Engelen <arnout@bzzt.net>
2026-05-18 10:17:13 +0200

    Make sure auth submodule is also loaded

--- src/asfquart/__init__.py
+++ src/asfquart/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # ensure all submodules are loaded
-from . import config, base, session, utils
+from . import auth, config, base, session, utils
 
 # This will be rewritten once construct() is called.
 APP = None
